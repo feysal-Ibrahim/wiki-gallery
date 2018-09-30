532a995f5e22bfbf43134ff20d8fd2e28c4f759c
feysal-Ibrahim <addictivefazman@gmail.com>
2018-09-30 11:36:23 +0300

    create modal for the app

--- app/models.py
+++ app/models.py
@@ -2,55 +2,6 @@ from django.db import models
 import datetime as dt
 
 # Create your models here.
-
-class Image(models.Model):
-    image = models.ImageField(upload_to = 'gallery/')
-    image_name = models.CharField(max_length=30)
-    image_url = models.TextField()
-    description = models.TextField(blank=True)
-    location = models.ForeignKey('Location', on_delete=models.CASCADE)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    posted_time = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ['posted_time']
-
-    def save_image(self):
-        self.save()
-
-    def delete_image(self):
-        self.delete()
-
-    @classmethod
-    def update_image(cls, id, target, update):
-        updated = cls.objects.filter(id=id).update(target=update)
-        return updated
-
-    @classmethod
-    def get_all(cls):
-        images = cls.objects.order_by('posted_time')
-        return images
-
-    @classmethod
-    def get_image(cls, id):
-        image = cls.objects.get(id=id)
-        return image
-
-    @classmethod
-    def search(cls, query):
-        result = cls.objects.filter(description__icontains=query).order_by('image_name')
-        return result
-
-    @classmethod
-    def search_by_category(cls, search_term):
-        display = cls.objects.filter(category__name__icontains=search_term)
-        return display
-
-    @classmethod
-    def filter_by_location(cls, search_term):
-        display = cls.objects.filter(location__name__icontains=search_term)
-        return display
-
 class Location(models.Model):
     name = models.CharField(max_length=60)
 
@@ -59,6 +10,9 @@ class Location(models.Model):
 
     def delete_locations(self):
         self.delete()
+
+    def update_locations(self):
+        self.update( )
 
     def __str__(self):
         return self.name
@@ -72,6 +26,95 @@ class Category(models.Model):
     def delete_category(self):
         self.delete()
 
+
+    def update_category(self):
+        self.update( )
+
+
     def __str__(self):
         return self.name
 
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'gallery/')
+    image_name = models.CharField(max_length=30)
+    image_url = models.TextField()
+    description = models.TextField(blank=True)
+    location = models.ForeignKey('Location', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    posted_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.image_name
+
+
+    class Meta:
+        ordering = ['posted_time']
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_image(self):
+        self.update( )
+
+    @classmethod
+    def get_all(cls):
+        images = cls.objects.order_by('posted_time')
+        return images
+
+    @classmethod
+    def search_by_category(cls , search_term):
+        image=cls.objects.filter( category__name__icontains=search_term )
+        return image
+
+    @classmethod
+    def search_by_city(cls , search_term):
+        image=cls.objects.filter( city__city__icontains=search_term )
+        return image
+
+    @classmethod
+    def get_image_by_id(cls):
+        Image.cls.objects.filter( id )
+        return Image
+    # @classmethod
+    # def search(cls, query):
+    #     result = cls.objects.filter(description__icontains=query).order_by('image_name')
+    #     return result
+    #
+    # @classmethod
+    # def search_by_category(cls, category):
+    #     image=cls.objects.get ( pk=category )
+    #     return image
+    #
+    # @classmethod
+    # def filter_by_location(cls, search_term):
+    #     locations = cls.objects.filter(location__name__icontains=search_term)
+    #     return locations
+    #
+    # @classmethod
+    # def filter_by_location(cls , location):
+    #     locations=cls.objects.filter (location=location )
+    #     return locations
+    #
+    # @classmethod
+    # def search_by_title(cls , search_term):
+    #     images=cls.objects.filter( image_name__icontains=search_term )
+    #     return images
+    #
+    # @classmethod
+    # def search_by_(cls , search_term):
+    #     images=cls.objects.filter ( image_name__icontains=search_term )
+    #     return images
+    #
+    #
+    #
+    # @classmethod
+    # def search_image(cls , category):
+    #     categories=cls.objects.filter (category=category )
+    #     return categories
+    #
+    # def get_image_by_id(self , id):
+    #     pass
+    #
--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url  # import the url function from the from the django.conf.urls
-from . import views #import the app's views module.
+from . import views  # import the app's views module.
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url ( r'^$', views.welcome , name='welcome' ),
-    url(r'^about/', views.about , name='about'),
+    url ( r'^$' , views.home , name='welcome' ) ,
+    url ( r'^about/' , views.about , name='about' ) ,
+    url( r'^search/' , views.search_results , name='search_results' ) ,
+    url( r'^picture/(\d+)' , views.single_image , name='filter' )
 ]
+if settings.DEBUG:
+    urlpatterns+=static ( settings.MEDIA_URL , document_root=settings.MEDIA_ROOT )
