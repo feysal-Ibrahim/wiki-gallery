b06f1c3ca8e48bfafaf7fa63e572bcdaa96da2a3
feysal-Ibrahim <addictivefazman@gmail.com>
2018-09-30 11:49:44 +0300

    create views for the about,serach,welcome page

--- app/views.py
+++ app/views.py
@@ -1,40 +1,40 @@
-# from django.shortcuts import render
-# from django.http import HttpResponse , Http404  # import the HttpResponse class from the django.http module.
-# from .models import Image , Category , Location
-# import datetime as dt
-# from peewee import DoesNotExist
-#
-#
-# # Create your view functios  here.
-# def home(request):
-#     # return render ( request , 'welcome.html')
-#     # gallery=Image.get_all( )
-#     images = Image.objects.filter().order_by('category')
-#     date=dt.date.today
-#     return render( request , 'welcome.html' ,
-#                    {"date": date , "images": images } )
-#
-#
-# def about(request):
-#     return render( request , 'about.html' )
-#
-#
-# def single_image(request , image_id):
-#     image=Image.objects.get( id=image_id )
-#     return render( request , 'filter.html' , {'image': image} )
-#
-#
-# def search_results(request):
-#     if 'category' in request.GET and request.GET["category"]:
-#         search_term=request.GET.get( "category" )
-#         searched_category=Image.search_by_category( search_term )
-#         message=f"{search_term}"
-#
-#         return render( request , 'search.html' , {"message": message , "categorys": searched_category} )
-#
-#     else:
-#         message="You haven't searched for any term"
-#         return render( request , 'search.html' , {"message": message} )
-#
-#
-#
+from django.shortcuts import render
+from django.http import HttpResponse , Http404  # import the HttpResponse class from the django.http module.
+from .models import Image , Category , Location
+import datetime as dt
+from peewee import DoesNotExist
+
+
+# Create your view functios  here.
+def home(request):
+    # return render ( request , 'welcome.html')
+    # gallery=Image.get_all( )
+    images = Image.objects.filter().order_by('category')
+    date=dt.date.today
+    return render( request , 'welcome.html' ,
+                   {"date": date , "images": images } )
+
+
+def about(request):
+    return render( request , 'about.html' )
+
+
+def single_image(request , image_id):
+    image=Image.objects.get( id=image_id )
+    return render( request , 'filter.html' , {'image': image} )
+
+
+def search_results(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search_term=request.GET.get( "category" )
+        searched_category=Image.search_by_category( search_term )
+        message=f"{search_term}"
+
+        return render( request , 'search.html' , {"message": message , "categorys": searched_category} )
+
+    else:
+        message="You haven't searched for any term"
+        return render( request , 'search.html' , {"message": message} )
+
+
+
