b14242d159a05941418def11e2fce24d09af43d8
feysal-Ibrahim <addictivefazman@gmail.com>
2018-10-01 16:51:55 +0300

    update virtual

--- gallery/settings.py
+++ gallery/settings.py
@@ -19,7 +19,7 @@ DEBUG=config( 'DEBUG' , default=False , cast=bool )
 if config( 'MODE' ) == "dev":
     DATABASES={
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2' ,
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': config( 'DB_NAME' ) ,
             'USER': config( 'DB_USER' ) ,
             'PASSWORD': config( 'DB_PASSWORD' ) ,
--- virtual/bin/django-admin.py
+++ virtual/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/feisal/gallery/virtual/bin/python2
+#!/home/feisal/gallery/virtual/bin/python
 from django.core import management
 
 if __name__ == "__main__":
